486765bbf7c05900568c2488959ee0e3825ad07a
ivanlin69 <117963772+ivanlin69@users.noreply.github.com>
2024-07-11 21:32:56 +0800

    Added bubble sort

--- Sorting/Sorting.h
+++ Sorting/Sorting.h
@@ -4,6 +4,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void Swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
+// # of comparison = n^2
+// # (max) of swap = n^2
+void BubbleSort(int *A, int n){
+    int count = 0;
+    int sorted = 1;  // for adaptivity
+    while(count < n){
+        sorted = 1;
+        for(size_t i=0; i<n-1; i++){
+            if(A[i] > A[i+1]){
+                Swap(&A[i], &A[i+1]);
+                sorted = 0;
+            }
+        }
+        if(sorted){
+            break;
+        }
+        count++;
+    }
+}
 
 #endif /* Sorting_h */
--- Sorting/main.c
+++ Sorting/main.c
@@ -4,7 +4,16 @@
 
 int main(int argc, const char * argv[]) {
     
+    int A[] = {8, 5, 7, 3, 2, 16, 13, 29};
+    int B[] = {2, 3, 5, 7, 8, 13, 16, 29};
+    int length = 8;
     
+    BubbleSort(B, length);
+    
+    for(size_t i=0; i<length; i++){
+        printf("%d ", A[i]);
+    }
+    printf("\n");
 
     return 0;
 }
