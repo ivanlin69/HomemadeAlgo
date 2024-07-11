bcbbba03425ba2eb7150101bf6d338e001a77f25
ivanlin69 <117963772+ivanlin69@users.noreply.github.com>
2024-07-11 21:49:19 +0800

    Finished for Bubble sort

--- Sorting/Sorting.h
+++ Sorting/Sorting.h
@@ -13,6 +13,8 @@ void Swap(int *a, int *b){
 // # of comparison = n^2
 // # (max) of swap = n^2
 // extra memories are not required
+// min time: O(n) if sorted, O(n^2) for the worst case
+// it's stable(order of duplicates are not changed)
 void BubbleSort(int *A, int n){
     int count = 0;
     int sorted = 1;  // for adaptivity
--- Sorting/main.c
+++ Sorting/main.c
@@ -4,8 +4,8 @@
 
 int main(int argc, const char * argv[]) {
     
-    int A[] = {8, 5, 7, 3, 2, 16, 13, 29};
-    int B[] = {2, 3, 5, 7, 8, 13, 16, 29};
+    int A[] = {8, 5, 16, 3, 2, 16, 13, 29};
+    int B[] = {3, 5, 7, 8, 13, 16, 16, 29};
     int length = 8;
     
     BubbleSort(A, length);
