2823343e051a38eae23733ca98745e460cad04b3
ivanlin69 <117963772+ivanlin69@users.noreply.github.com>
2024-07-12 11:36:29 +0800

    Added Insertion sort

--- Sorting/Sorting.h
+++ Sorting/Sorting.h
@@ -35,4 +35,29 @@ void BubbleSort(int *A, int n){
     }
 }
 
+// # of comparison = n^2
+// # (max) of swap = n^2
+// extra memories are not required
+// min time: O(n^2) if sorted, O(n^2) for the worst case
+// it's stable(order of duplicates are not changed)
+// it's not adaptive
+void InsertionSort(int *A, int n){
+    
+    if(n == 1){
+        return;
+    }
+    
+    // for each element(except for the first), compare it with the sorted portion
+    for(size_t i=1; i<n; i++){
+        size_t j=i;
+        // don't use another for loop, use while so it can terminate without compare all elements
+        // in the sorted portion, and thus it's adaptive
+        while(j>0 && A[j] < A[j-1]){
+            Swap(&A[j], &A[j-1]);
+            j--;
+        }
+    }
+}
+
+
 #endif /* Sorting_h */
--- Sorting/main.c
+++ Sorting/main.c
@@ -8,7 +8,8 @@ int main(int argc, const char * argv[]) {
     int B[] = {3, 5, 7, 8, 13, 16, 16, 29};
     int length = 8;
     
-    BubbleSort(A, length);
+    //BubbleSort(A, length);
+    InsertionSort(A, length);
     
     for(size_t i=0; i<length; i++){
         printf("%d ", A[i]);
